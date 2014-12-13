1455ffdaa0a17ce6b880077b6d963212f6211a0e
deandungo <dean@forctym-mbp.local>
2014-12-13 16:28:41 +0000

    Cleanup of code
    Added JavaDoc to Lists.java
    Cleaned up code formatting, removed used of the i variable for the for
    loop (replaced with counter).
    Cleaned up console output, easier to tell which values are before and
    after the use of ListIterator now.
    Commented out package because of the lack of package-info and the file
    is not within the package folder.

--- Lists.java
+++ Lists.java
@@ -1,20 +1,51 @@
-package cal;
+//package cal;
 
 import java.util.LinkedList;
 import java.util.ListIterator;
+
+/**
+ * This class iterates through a LinkedList, adding linearly incrementing values
+ * to each consecutive element, and then outputting the values onto a console.
+ * 
+ * This has been forked from guojinchen's helloworld repository by deandungo.
+ * 
+ * @author guojinchen
+ *
+ */
 public class Lists {
- public static void main(String[] args)   {
-	LinkedList  linklist = new LinkedList();
-	
-	for(int i=0;i<10;i++){
-		int a = (i+1)/2;
-	 System.out.println(a);
-	linklist.add(a, i);
-	 
-}
-	 ListIterator it =  linklist.listIterator(0);
-	      while(it.hasNext()){
-	    	  System.out.println("I"+it.next());    	 
-	      }
- }
+
+	/**
+	 * Default main class.
+	 */
+	public static void main(String[] args) {
+
+		/**
+		 * Stores the various values.
+		 */
+		LinkedList linkList = new LinkedList();
+
+		for (int elementCounter = 0; elementCounter < 10; elementCounter++) {
+			int elementValue = ((elementCounter + 1) / 2);
+			System.out.println("Value for element " + elementCounter + ": " + elementValue);
+			linkList.add(elementValue, elementCounter);
+		}
+
+        System.out.println(" - List iteration begins here - ");
+
+		/**
+		 * Allows iteration through the LinkedList elements.
+		 */
+		ListIterator listIterator = linkList.listIterator(0);
+
+		/**
+		 * Keeps track of the current element.
+		 */
+		int listCounter = 0;
+
+		while (listIterator.hasNext()) {
+			System.out.println("Value for element " + listCounter++ + ": " + listIterator.next());
+		}
+
+	}
+
 }
