188f7ff39e26555fe1a1ab66b319dcea30bfd099
Zevrix Kojote <zevrixkojote@gmail.com>
2015-02-26 21:22:20 -0500

    Code Formatting and Supplied Types to LinkedList

--- Lists.java
+++ Lists.java
@@ -2,19 +2,25 @@ package cal;
 
 import java.util.LinkedList;
 import java.util.ListIterator;
-public class Lists {
- public static void main(String[] args)   {
-	LinkedList  linklist = new LinkedList();
-	
-	for(int i=0;i<10;i++){
-		int a = (i+1)/2;
-	 System.out.println(a);
-	linklist.add(a, i);
-	 
-}
-	 ListIterator it =  linklist.listIterator(0);
-	      while(it.hasNext()){
-	    	  System.out.println("I"+it.next());    	 
-	      }
- }
+
+public class Lists 
+{
+	public static void main(String[] args)
+	{
+		LinkedList<Integer> linklist = new LinkedList<Integer>();
+    	
+		for(int i = 0; i < 10; i++) 
+		{
+			int a = (i+1)/2;
+			System.out.println(a);
+			linklist.add(a, i);
+		}
+    	
+		ListIterator<Integer> it =  linklist.listIterator(0);
+
+		while(it.hasNext())
+		{
+			System.out.println("I" + it.next());    	 
+		}
+	}
 }
